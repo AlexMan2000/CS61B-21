964cce559a210cc384a8cc5e48b2c9eda5214be3
Alex Jiasheng Ni <niallni1980@gmail.com>
2024-07-22 09:14:18 -0400

    20240722

---
+++ playground/Threading.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Threading {
+
+    private List<String> data = new ArrayList<>();
+    private Set<String> dat = new HashSet<>();
+
+
+    public String getFromIndex(int index) {
+        return data.get(index);
+    }
+
+
+
+    public static void main(String[] args) {
+        String[] tmp = new String[4];
+    }
+}
+
+
+
